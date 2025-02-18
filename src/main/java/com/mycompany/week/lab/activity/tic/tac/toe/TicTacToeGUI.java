52018f15c404e903cdb01ac9b82799819490739c
Sophia Golden <sophia.golden28@houghton.edu>
2025-02-18 13:12:36 -0500

    Completed Console and GUI version

--- src/main/java/com/mycompany/week/lab/activity/tic/tac/toe/TicTacToeGUI.java
+++ src/main/java/com/mycompany/week/lab/activity/tic/tac/toe/TicTacToeGUI.java
@@ -18,6 +18,10 @@ public class TicTacToeGUI extends javax.swing.JFrame {
      */
     public TicTacToeGUI() {
         initComponents();
+
+        //disable the restart button:
+        ButtonRestart.setVisible(false);
+
     }
 
     //variable declaration
@@ -87,11 +91,13 @@ public class TicTacToeGUI extends javax.swing.JFrame {
             if (currentPlayer.equals("X")) {
 
                 LabelPlayerTurnWin.setText("Player One has won the game!");
+                ButtonRestart.setVisible(true);
                 return true;
 
             } else {
 
                 LabelPlayerTurnWin.setText("Player Two has won the game!");
+                ButtonRestart.setVisible(true);
                 return true;
 
             }
@@ -102,6 +108,7 @@ public class TicTacToeGUI extends javax.swing.JFrame {
         if (checkForWinner() == false && checkForFull() == true) {
 
             LabelPlayerTurnWin.setText("The game is a tie!");
+            ButtonRestart.setVisible(true);
             return true;
 
         }
@@ -238,6 +245,9 @@ public class TicTacToeGUI extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jMenuBar1 = new javax.swing.JMenuBar();
+        jMenu1 = new javax.swing.JMenu();
+        jMenu2 = new javax.swing.JMenu();
         jPanel1 = new javax.swing.JPanel();
         jLabel1 = new javax.swing.JLabel();
         jPanel2 = new javax.swing.JPanel();
@@ -250,7 +260,14 @@ public class TicTacToeGUI extends javax.swing.JFrame {
         ButtonSpace7 = new javax.swing.JButton();
         ButtonSpace8 = new javax.swing.JButton();
         ButtonSpace9 = new javax.swing.JButton();
+        ButtonRestart = new javax.swing.JButton();
         LabelPlayerTurnWin = new javax.swing.JLabel();
+
+        jMenu1.setText("File");
+        jMenuBar1.add(jMenu1);
+
+        jMenu2.setText("Edit");
+        jMenuBar1.add(jMenu2);
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -356,7 +373,7 @@ public class TicTacToeGUI extends javax.swing.JFrame {
         jPanel2Layout.setHorizontalGroup(
             jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel2Layout.createSequentialGroup()
-                .addContainerGap(50, Short.MAX_VALUE)
+                .addContainerGap(202, Short.MAX_VALUE)
                 .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
                     .addComponent(ButtonSpace7, javax.swing.GroupLayout.PREFERRED_SIZE, 0, Short.MAX_VALUE)
                     .addComponent(ButtonSpace4, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.PREFERRED_SIZE, 0, Short.MAX_VALUE)
@@ -375,7 +392,7 @@ public class TicTacToeGUI extends javax.swing.JFrame {
                         .addComponent(ButtonSpace8, javax.swing.GroupLayout.PREFERRED_SIZE, 44, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addGap(50, 50, 50)
                         .addComponent(ButtonSpace9, javax.swing.GroupLayout.PREFERRED_SIZE, 0, Short.MAX_VALUE)))
-                .addGap(59, 59, 59))
+                .addContainerGap(150, Short.MAX_VALUE))
         );
         jPanel2Layout.setVerticalGroup(
             jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -398,6 +415,18 @@ public class TicTacToeGUI extends javax.swing.JFrame {
                 .addContainerGap(19, Short.MAX_VALUE))
         );
 
+        ButtonRestart.setText("Play again?");
+        ButtonRestart.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                ButtonRestartMouseClicked(evt);
+            }
+        });
+        ButtonRestart.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                ButtonRestartActionPerformed(evt);
+            }
+        });
+
         LabelPlayerTurnWin.setFont(new java.awt.Font("Segoe UI", 1, 24)); // NOI18N
         LabelPlayerTurnWin.setForeground(new java.awt.Color(0, 0, 0));
         LabelPlayerTurnWin.setText("Player 1's turn!");
@@ -406,34 +435,45 @@ public class TicTacToeGUI extends javax.swing.JFrame {
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 183, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(89, 89, 89))
             .addGroup(jPanel1Layout.createSequentialGroup()
-                .addGap(17, 17, 17)
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                    .addComponent(jPanel2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                    .addComponent(LabelPlayerTurnWin, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addContainerGap()
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addComponent(LabelPlayerTurnWin, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addGap(122, 122, 122)
+                        .addComponent(ButtonRestart)
+                        .addGap(219, 219, 219))
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 288, Short.MAX_VALUE)
+                        .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 183, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addContainerGap(288, Short.MAX_VALUE))))
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGap(60, 60, 60)
+                .addComponent(jPanel2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(0, 0, Short.MAX_VALUE))
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel1Layout.createSequentialGroup()
                 .addGap(15, 15, 15)
                 .addComponent(jLabel1)
-                .addGap(18, 18, 18)
-                .addComponent(jPanel2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addComponent(LabelPlayerTurnWin)
-                .addContainerGap(23, Short.MAX_VALUE))
+                .addComponent(jPanel2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(18, 18, 18)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(LabelPlayerTurnWin)
+                    .addComponent(ButtonRestart))
+                .addContainerGap(20, Short.MAX_VALUE))
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(0, 0, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -471,14 +511,14 @@ public class TicTacToeGUI extends javax.swing.JFrame {
 
         //if the spot isn't taken set the text to the current player's symbol and switch players
         if (!ButtonSpace1.getText().equals("X") && !ButtonSpace1.getText().equals("O")) {
-            
+
             //if the game isn't already over we can move
-            if (checkEndCondition() == false){
+            if (checkEndCondition() == false) {
 
                 ButtonSpace1.setText(currentPlayer);
 
             }
-            
+
             //if it's not an end condition we switch players
             //we don't need the string of disabling the buttons !! 
             //the code would work if it were just if checkEndCondition() == false {switchplayers}
@@ -783,6 +823,41 @@ public class TicTacToeGUI extends javax.swing.JFrame {
         }
     }//GEN-LAST:event_ButtonSpace9MouseClicked
 
+    private void ButtonRestartActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_ButtonRestartActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_ButtonRestartActionPerformed
+
+    private void ButtonRestartMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_ButtonRestartMouseClicked
+        // TODO add your handling code here:
+
+        //restart
+        currentPlayer = "X";
+        ButtonSpace1.setEnabled(true);
+        ButtonSpace2.setEnabled(true);
+        ButtonSpace3.setEnabled(true);
+        ButtonSpace4.setEnabled(true);
+        ButtonSpace5.setEnabled(true);
+        ButtonSpace6.setEnabled(true);
+        ButtonSpace7.setEnabled(true);
+        ButtonSpace8.setEnabled(true);
+        ButtonSpace9.setEnabled(true);
+
+        ButtonSpace1.setText(" ");
+        ButtonSpace2.setText(" ");
+        ButtonSpace3.setText(" ");
+        ButtonSpace4.setText(" ");
+        ButtonSpace5.setText(" ");
+        ButtonSpace6.setText(" ");
+        ButtonSpace7.setText(" ");
+        ButtonSpace8.setText(" ");
+        ButtonSpace9.setText(" ");
+        
+        //hide the button again
+        ButtonRestart.setVisible(false);
+
+
+    }//GEN-LAST:event_ButtonRestartMouseClicked
+
     /**
      * @param args the command line arguments
      */
@@ -820,6 +895,7 @@ public class TicTacToeGUI extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton ButtonRestart;
     private javax.swing.JButton ButtonSpace1;
     private javax.swing.JButton ButtonSpace2;
     private javax.swing.JButton ButtonSpace3;
@@ -831,6 +907,9 @@ public class TicTacToeGUI extends javax.swing.JFrame {
     private javax.swing.JButton ButtonSpace9;
     private javax.swing.JLabel LabelPlayerTurnWin;
     private javax.swing.JLabel jLabel1;
+    private javax.swing.JMenu jMenu1;
+    private javax.swing.JMenu jMenu2;
+    private javax.swing.JMenuBar jMenuBar1;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JPanel jPanel2;
     // End of variables declaration//GEN-END:variables
